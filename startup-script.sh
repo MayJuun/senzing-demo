8417031011bee903dc752f5afcb3524659ea7caf
ZapsApps <49655139+ZapsApps@users.noreply.github.com>
2021-06-30 01:05:44 -0400

    Update startup-script.sh
    adding synthea install to VM

--- startup-script.sh
+++ startup-script.sh
@@ -37,6 +37,11 @@ sudo apt install docker.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# Clone Synthea GH repo
+mkdir --parents ~/synthea.git
+cd ~/synthea.git
+git clone  --recurse-submodules https://github.com/synthetichealth/synthea.git
+
 # Clone senzing GH repo
 mkdir --parents ${GIT_ACCOUNT_DIR}
 cd  ${GIT_ACCOUNT_DIR}
